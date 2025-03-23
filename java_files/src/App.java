cd3b19922779dc52c017396118bbd679abc5d604
asus <3311614415@qq.com>
2025-03-23 14:46:40 +0800

    1

--- java_files/src/App.java
+++ java_files/src/App.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+import java.util.*;
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static int kthGrammar(int n, int k) {
+        if(n==1&&k==1) return 0;
+        if(k>Math.pow(2,n-2)){
+          return anti(kthGrammar(n,k-(int)Math.pow(2,n-2)));
+        }
+        return kthGrammar(n-1,k);
+      }
+    
+      public static int anti(int a){
+        return a==1?0:1;
+      }
+    public static void main(String[] args) {
+        System.out.println(kthGrammar(3,3));
+    
     }
 }
